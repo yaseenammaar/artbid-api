680242f3c2b7c6a1c6c9dc1912512f2fca27919d
Shubham singh <proshubham5@gmail.com>
2021-04-10 16:26:04 +0530

    major bugs fixed; uploadItems api tested

--- functions/src/protectedApp/itemUpload.ts
+++ functions/src/protectedApp/itemUpload.ts
@@ -15,7 +15,7 @@ interface mResponse {
 }
 
 //Function that handles the upload item api
-const itemUpload = async (req : customRequest, res : Response) => {
+const itemUpload = async (req: customRequest, res: Response) => {
     try {
 
         const {
@@ -33,51 +33,58 @@ const itemUpload = async (req : customRequest, res : Response) => {
             supporting_images,
         } = req.body
 
-        const search_tags = [title, state, category, caption]
+        const search_tags_demo: string[] = [title, state, category, caption]
+        console.log(search_tags_demo)
+        const search_tags: string[] = []
+        search_tags.push(title)
+        search_tags.push(state)
+        search_tags.push(category)
+        search_tags.push(caption)
+
+
         let search_permutations: string[] = []
 
         const itemCollection = db.collection('items')
         const keywordsCollection = db.collection('keywords')
 
         let itemRes = null
+        const batch = db.batch();
 
-        await db.runTransaction(async (t) => {
-
-            // take each tag and fill search_permutations with each possibility of search
-            for (let i = 0; i < search_tags.length; i ++) {
-                let keywordPermutation: string[] = []
-                const tag: string = search_tags[i];
-                for(let j = 1; j <= search_tags.length; j++) {
-                    const tagSubstr = tag.substr(0, j)
-                    search_permutations.push(tagSubstr);
-                    keywordPermutation.push(tagSubstr)
-                }
-
-                await keywordsCollection.doc(tag).set({
-                    key_map: keywordPermutation,
-                    score: 0,
-                })
+        // take each tag and fill search_permutations with each possibility of search
+        for (let i = 0; i < search_tags.length; i++) {
+            let keywordPermutation: string[] = []
+            const tag: string = search_tags[i];
+            console.log("search_tag : ", tag)
+            for (let j = 1; j <= tag.length; j++) {
+                const tagSubstr = tag.substr(0, j)
+                search_permutations.push(tagSubstr);
+                keywordPermutation.push(tagSubstr)
             }
 
-            itemRes = await itemCollection.add({
-                available_state,
-                base_price,
-                by_user,
-                caption,
-                category,
-                closing_date,
-                closing_time,
-                state,
-                status,
-                title,
-                featured_image,
-                supporting_images,
-                search_tags,
-                search_permutations,
+            batch.set(keywordsCollection.doc(tag), {
+                key_map: keywordPermutation,
+                score: 0,
             })
+        }
 
-
+        batch.set(itemCollection.doc(), {
+            available_state,
+            base_price,
+            by_user,
+            caption,
+            category,
+            closing_date,
+            closing_time,
+            state,
+            status,
+            title,
+            featured_image,
+            supporting_images,
+            search_tags,
+            search_permutations,
         })
+
+        itemRes = await batch.commit()
 
         const response: mResponse = {
             statusCode: 200,
@@ -89,6 +96,8 @@ const itemUpload = async (req : customRequest, res : Response) => {
         res.send(response)
 
     } catch (error) {
+        console.log(error)
+        console.log(error.message)
         const response: mResponse = {
             statusCode: 400,
             isError: true,
